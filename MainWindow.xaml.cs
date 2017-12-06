368c11e9a1be37b532bc2445a95502240e53f534
Joshua T <replicaJunction@users.noreply.github.com>
2017-12-06 10:43:08 -0600

    Create MainWindow.xaml.cs

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ReactiveTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, IViewFor<MainWindowViewModel>
+    {
+        public MainWindow()
+        {
+            MainWindowViewModel.Bootstrap();
+
+            InitializeComponent();
+            this.DataContext = new MainWindowViewModel();
+        }
+
+        public MainWindowViewModel ViewModel { get; set; }
+        object IViewFor.ViewModel
+        {
+            get => ViewModel;
+            set => ViewModel = (MainWindowViewModel)value;
+        }
+    }
+}
